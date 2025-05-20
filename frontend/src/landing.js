a05030f30ceb397e4940774e1cdbec58e43eedeb
NuwoMaaan <Leoholden24@outlook.com>
2025-05-20 22:27:57 +1000

    C# cleanup remains

---
+++ frontend/src/landing.js
@@ -0,0 +1,15 @@
+import React from "react";
+import { useState, useEffect } from "react";
+
+function Landing() {
+  const [data, setData] = useState([]);
+
+  return (
+    <div>
+      <h1>AWE Online Store</h1>
+        <h2>Welcome to the AWE Online Store</h2>
+    </div>
+  );
+}
+
+export default Landing;
