d82271a2ad8e8692c9f1469e15f32298cba04542
NuwoMaaan <Leoholden24@outlook.com>
2025-11-27 14:13:06 +1100

    fix accidental removal of to_dict method for Order

--- Backend/models/order.py
+++ Backend/models/order.py
@@ -26,4 +26,11 @@ class Order:
     def save_order_to_db(self, datetime):
         OrderService.save_order_to_db(self, datetime)
 
-    
+    def to_dict(self, datetime):
+        return {
+            "order_no": self.order_no,
+            "items": [{"item_id": item.item_id} for item in self.items],
+            "shipping_details": self.shipping_details,
+            "total": round(float(self.total), 2),
+            "date": datetime.strftime("%Y-%m-%d %H:%M:%S") if hasattr(datetime, "strftime") else str(datetime)
+        }
--- README.md
+++ README.md
@@ -3,3 +3,5 @@
 ## Overview
 
 OOP built backend using Python for an online store that has a terminal based interface for interaction.
+
+
