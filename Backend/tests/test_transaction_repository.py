ccaf26baedb852ab95c007cff28d64d18cd987c3
Leo Holden <Leoholden24@outlook.com>
2026-03-15 22:06:37 +1100

    implement pytest fixtures for repetitive tests

--- Backend/tests/test_transaction_repository.py
+++ Backend/tests/test_transaction_repository.py
@@ -1,14 +1,21 @@
+import pytest
+from decimal import Decimal
 from db.models import Orders, OrderItems
 from models.order import Order
 from models.item import Item
 from db.repositories.transaction_repository import insert_order, insert_order_items, reduce_stock
-from decimal import Decimal
 
 
-item = Item(id=1, name="Test Item", price=10.00)
-order_data = Order(
+@pytest.fixture
+def test_item():
+    return Item(id=1, name="Test Item", price=Decimal("10.00"))
+
+
+@pytest.fixture
+def test_order(test_item):
+    return Order(
         customer_id=1,
-        items=[item, item],
+        items=[test_item, test_item],
         shipping_details={
             "address": "123 Test St",
             "city": "Testville",
@@ -16,21 +23,23 @@ order_data = Order(
         }
     )
 
-def test_insert_order(db_session):
-    order_id = insert_order(order_data, db_session)
+
+def test_insert_order(db_session, test_order):
+    order_id = insert_order(test_order, db_session)
     assert isinstance(order_id, int)
-    
+
     order = db_session.query(Orders).filter_by(order_id=order_id).first()
     assert order is not None
     assert order.user_id == 1
-    assert order.total == Decimal("30.00")  # shipping + item price
+    assert order.total == Decimal("30.00")
     assert order.address == "123 Test St"
     assert order.city == "Testville"
     assert order.postal_code == "3000"
 
-def test_insert_order_items(db_session):
-    order_id = insert_order(order_data, db_session)
-    order_item_id = insert_order_items(order_id, order_data.items, db_session)
+
+def test_insert_order_items(db_session, test_order):
+    order_id = insert_order(test_order, db_session)
+    order_item_id = insert_order_items(order_id, test_order.items, db_session)
     assert isinstance(order_item_id, int)
 
     order_item = db_session.query(OrderItems).filter_by(order_id=order_id).first()
